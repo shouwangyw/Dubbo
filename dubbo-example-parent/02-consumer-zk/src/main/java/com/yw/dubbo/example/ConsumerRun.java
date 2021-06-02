fc31ed3884113904d3ee86fe4320031d09718f5d
yangwei <897900564@qq.com>
2021-06-02 22:00:46 +0800

    负载均衡

--- dubbo-example-parent/02-consumer-zk/src/main/java/com/yw/dubbo/example/ConsumerRun.java
+++ dubbo-example-parent/02-consumer-zk/src/main/java/com/yw/dubbo/example/ConsumerRun.java
@@ -13,7 +13,12 @@ public class ConsumerRun {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("spring-consumer.xml");
         SomeService service = (SomeService) ac.getBean("someService");
-        String hello = service.hello("China");
-        System.out.println(hello);
+//        String hello = service.hello("China");
+//        System.out.println(hello);
+
+        for (int i = 0; i < 10; i++) {
+            String hello = service.hello("Consumer-" + i);
+            System.out.println(hello);
+        }
     }
 }
--- dubbo-example-parent/02-provider-zk/src/main/java/com/yw/dubbo/example/provider/SomeServiceImpl.java
+++ dubbo-example-parent/02-provider-zk/src/main/java/com/yw/dubbo/example/provider/SomeServiceImpl.java
@@ -12,4 +12,25 @@ public class SomeServiceImpl implements SomeService {
         System.out.println(name + "，我是提供者");
         return "Hello Dubbo World! " + name;
     }
+
+    /**
+     * 下面代码用于演示负载均衡
+     */
+//    @Override
+//    public String hello(String name) {
+//        System.out.println("执行【第一个】提供者的hello() " + name);
+//        return "【第一个】提供者";
+//    }
+
+//    @Override
+//    public String hello(String name) {
+//        System.out.println("执行【第二个】提供者的hello() " + name);
+//        return "【第二个】提供者";
+//    }
+
+//    @Override
+//    public String hello(String name) {
+//        System.out.println("执行【第三个】提供者的hello() " + name);
+//        return "【第三个】提供者";
+//    }
 }
