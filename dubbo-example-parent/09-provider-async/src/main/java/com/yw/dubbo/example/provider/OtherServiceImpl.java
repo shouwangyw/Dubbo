9add5c1aaacb228f192745c1c0971076d28386fa
yangwei <897900564@qq.com>
2021-06-04 01:20:58 +0800

    提供者的异步执行

--- dubbo-example-parent/09-provider-async/src/main/java/com/yw/dubbo/example/provider/OtherServiceImpl.java
+++ dubbo-example-parent/09-provider-async/src/main/java/com/yw/dubbo/example/provider/OtherServiceImpl.java
@@ -33,15 +33,39 @@ public class OtherServiceImpl implements OtherService {
 //        return "doFourth()";
 //    }
 
+//    @Override
+//    public CompletableFuture<String> doThird() {
+//        sleep("doThird");
+//        return CompletableFuture.completedFuture("doThird()");
+//    }
+//    @Override
+//    public CompletableFuture<String> doFourth() {
+//        sleep("doFourth");
+//        return CompletableFuture.completedFuture("doFourth()");
+//    }
+
+    /**
+     * 提供者进行异步执行
+     */
     @Override
     public CompletableFuture<String> doThird() {
-        sleep("doThird");
-        return CompletableFuture.completedFuture("doThird()");
+        long startTime = System.currentTimeMillis();
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            sleep("doThird");
+            return "doThird";
+        });
+        System.out.println("doThird()方法执行耗时(毫秒)：" + (System.currentTimeMillis() - startTime));
+        return future;
     }
     @Override
     public CompletableFuture<String> doFourth() {
-        sleep("doFourth");
-        return CompletableFuture.completedFuture("doFourth()");
+        long startTime = System.currentTimeMillis();
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            sleep("doFourth");
+            return "doFourth";
+        });
+        System.out.println("doFourth()方法执行耗时(毫秒)：" + (System.currentTimeMillis() - startTime));
+        return future;
     }
 
     // 模拟耗时操作
