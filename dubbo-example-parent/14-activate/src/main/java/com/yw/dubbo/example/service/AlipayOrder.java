5d93faf296ce508bd4954f43494fab4a48bb9995
yangwei <897900564@qq.com>
2021-06-15 00:18:49 +0800

    Dubbo 笔记

--- dubbo-example-parent/14-activate/src/main/java/com/yw/dubbo/example/service/AlipayOrder.java
+++ dubbo-example-parent/14-activate/src/main/java/com/yw/dubbo/example/service/AlipayOrder.java
@@ -5,7 +5,9 @@ import org.apache.dubbo.common.extension.Activate;
 /**
  * @author yangwei
  */
-@Activate(group = "online")
+//@Activate(group = "online")
+// 若 group 属性和 value 属性同时存在，则是“与”的结果
+@Activate(group = "online", value = "alipay")
 public class AlipayOrder implements Order {
     @Override
     public String way() {
